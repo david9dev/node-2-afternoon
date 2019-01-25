3046dca72af06d1e2fdb1fab1c23b1cb6847cdd1
David Wells <davidwells@Davids-MacBook-Pro.local>
2019-01-24 18:04:50 -0700

    finished

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,5 +1,83 @@
 
 
 module.exports = {
-    
+
+    getOne: function(request, response)
+    {
+        const {id} = request.params;
+        // console.log(request);
+        request.app.get('db').read_product(id)
+        // i dont really know what exactly this line is doing, i think
+        .then((product) =>
+        {
+            if(product[0])
+            response.status(200).send(product)//send(product)
+            else response.status(400).json('coudnt find product')
+        })
+        .catch((error) =>
+        {
+            console.log('error', error);
+            response.status(500).json('couldnt get product');
+        })
+    },
+    getAll: function(request, response)
+    {
+        request.app.get('db').read_products()
+        .then((products) =>
+        {
+            response.status(200).send(products);
+        })
+        .catch((error) =>
+        {
+            console.log('error', error);
+            response.status(500).json('couldnt get products');
+        })
+
+    },
+
+
+    create: function(request, response)
+    {
+        const {name, description, price, image_url} = request.body
+        request.app.get('db').create_product([name, description, price, image_url])
+        .then(() =>
+        {
+            response.status(200).json("Product Added");
+        })
+        .catch((error) =>
+        {
+            console.log("error", error);
+            response.status(500).send("error");
+        })
+    },
+    update: function(request, response)
+    {
+        const {desc} = request.query;
+        const {id} = request.params;
+        request.app.get('db').update_product(desc, id)
+        .then(() =>
+        {
+            response.status(200).json("updated product");
+        })
+        .catch((error) =>
+        {
+            console.log("error", error);
+            response.status(500).json("couldnt update product");
+        })
+    },
+    delete: function(request, response)
+    {
+        const {id} = request.params;
+        request.app.get('db').delete_product(id)
+        .then(() =>
+        {
+            response.status(200).json("product deleted");
+        })
+        .catch((error) =>
+        {
+            console.log("error", error);
+            response.status(500).json("unable to delete product");
+        });
+    }
+
 }
--- server/index.js
+++ server/index.js
@@ -8,16 +8,26 @@ const app = express();
 app.use(bodyParser.json());
 massive(process.env.CONNECTION_STRING).then((db) => 
 //Question: where does the .then come from and how does it get information form
-// its attached function?
+// its attached function?// from the proto_type.promise architecture
 //the connection string in the first argument of massive() is to set up where
 // massive is looking for the data base that it is going to be
 // doing all of the requests and functions for.
 {
     app.set('db', db);
 })
+.catch((error) =>
+{
+    console.log("error", error);
+})
 // i dont really understand what that line is doing. im guessing that it is 
 // creating an object variable with-in app called 'db' with the object value of db
 // which comes from the object returned form the massive() invokation;
+
+app.get('/api/products/:id',contrlr.getOne);
+app.get('/api/products', contrlr.getAll);
+app.post('/api/products', contrlr.create);
+app.delete('/api/products/:id', contrlr.delete);
+app.put('/api/products/:id', contrlr.update);
 
 
 const port = process.env.PORT;
